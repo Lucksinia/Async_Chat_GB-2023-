840a81a40a0c4ca193f0c787a70bc1553ee0a919
Lucksinia <denni334334@gmail.com>
2023-04-28 16:11:38 +0300

    added client test suite.

---
+++ lesson_4/client_test.py
@@ -0,0 +1,44 @@
+import json
+from unittest import TestCase, main
+from lesson_3.client import presence, get_addr_port, read_answer
+
+
+# тестовые данные
+test_data = {
+    "action": "presence",
+    "time": 1.123,
+    "type": "status",
+    "user": {"account_name": "account_name", "status": "status"},
+}
+
+
+def for_test_msg_to_client(d):
+    if d["action"] == "presence":
+        msg = {"response": 200, "time": 1.123, "alert": "All clear"}
+        msg = json.dumps(msg, indent=4).encode("utf-8")
+        return msg
+
+
+class TestClient(TestCase):
+    def test_presence(self):
+        test_result = presence(account_name="account_name", status="status")
+        test_result["time"] = 1.123  # принудительно time = 1.123
+        self.assertEqual(test_result, test_data)
+
+    def test_get_addr_port(self):
+        self.assertEqual(
+            str(get_addr_port()),
+            "Namespace(addr='localhost', port=7777, user='Varvara', status='2 "
+            "years')",
+        )
+
+    def test_read_answer(self):
+        # имитируем отправку ответа сервера
+        test_result = json.loads(for_test_msg_to_client(test_data))
+        expected_output = read_answer(test_result)
+        actual_result = (200, "All clear")  # ожидаемый результат
+        self.assertEqual(expected_output, actual_result)
+
+
+if __name__ == "__main__":
+    main()
