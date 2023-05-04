be7ae9f235122247c85d7edcd692856417d2cc08
Lucksinia <denni334334@gmail.com>
2023-05-04 15:35:33 +0300

    added client messaging ability

---
+++ startscript.py
@@ -0,0 +1,22 @@
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+client_list = []
+while True:
+    user = input("start test clients(s) / Close test clients (x) / Quit(q): ")
+    if user == "q":
+        break
+    elif user == "s":
+        client_list.append(
+            Popen(
+                "python lesson_5\client.py --user NotMe --send 1", creationflags=CREATE_NEW_CONSOLE
+            )
+        )
+        client_list.append(Popen("python lesson_5\client.py", creationflags=CREATE_NEW_CONSOLE))
+        client_list.append(
+            Popen("python lesson_5\client.py --user Daniil", creationflags=CREATE_NEW_CONSOLE)
+        )
+        print(f"Started {len(client_list)}")
+    elif user == "x":
+        for p in client_list:
+            p.kill()
+        client_list.clear()
