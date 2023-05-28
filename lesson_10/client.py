b760c7530bdd81e9a062610e12724eeeef3be584
Lucksinia <denni334334@gmail.com>
2023-05-28 23:20:22 +0300

    classified client

--- lesson_10/client.py
+++ lesson_10/client.py
@@ -19,42 +19,147 @@ Client: s = socket() ...
 """
 
 
-@log
-def presence(account_name, status):
-    data = {
-        "action": "presence",
-        "time": datetime.timestamp(datetime.now()),
-        "type": "status",
-        "user": {"account_name": account_name, "status": status},
-    }
-    return data
+class ClientVerifier(type):
+    def __init__(cls, classname, bases, dict):
+        for func in dict:
+            try:
+                instructions = get_instructions(dict[func])
+            except TypeError:
+                pass
+            else:
+                for op in instructions:
+                    if op.opname == "LOAD_GLOBAL":
+                        if op.argval in ("accept", "listen", "socket"):
+                            raise TypeError(f"{op.argval} cant use in class Client")
+                    if op.opname == "LOAD_METHOD":
+                        if op.argval == "create_socket_client":
+                            raise TypeError(f"Required to not create sockets in class")
+
+        super().__init__(classname, bases, dict)
+
+
+class Client(metaclass=ClientVerifier):
+    def __init__(self, s, user, status):
+        self.s = s
+        self.user = user
+        self.status = status
+
+    def run(self):  # main function
+        presence_msg = self.presence(self.user, self.status)
+        client_log.info("Presence message sending start...")
+        self.send(json.dumps(presence_msg, indent=4).encode("utf-8"))
+        client_log.info("Presence message sending...DONE")
+        try:
+            data = self.data_from_server(self.s)
+            answer = self.read_answer(data)
+            print(f"Message(answer) from server received: ", answer)
+        except:
+            client_log.error("No messages from server.")
+            exit(1)
+
+        w = Thread(target=self.chat_w_message, args=(self.user, self.s))
+        w.daemon = True
+        w.start()
+
+        r = Thread(target=self.chat_r_message, args=(self.s,))
+        r.daemon = True
+        r.start()
+
+        while True:
+            time.sleep(1)
+            if not w.is_alive() or not r.is_alive():
+                break
+
+    @staticmethod
+    def data_from_server(sock):
+        received_data = sock.recv(10000000)
+        decoded_data = received_data.decode("utf-8")
+        result_data = json.loads(decoded_data)
+        if type(result_data) == dict:
+            return result_data
+        return {}
+
+    @staticmethod
+    def read_answer(dict_from_server):
+        try:
+            response_code = dict_from_server.get("response", 0)
+        except Exception as err:
+            client_log.error(f"Error {err})")
+        else:
+            if response_code:
+                if response_code == 200:
+                    result_msg = dict_from_server.get("alert", "OK.")
+                    client_log.info("Response status OK")
+                    try:
+                        result_msg = dict_from_server.get("text", 0)
+                    except Exception as err:
+                        client_log.error(f"Error {err})")
+                    else:
+                        if result_msg:
+                            return f">>>{result_msg[1]}: {result_msg[0]}"
+                else:
+                    result_msg = dict_from_server.get("error", "unknown error")
+                    client_log.info(f"Status: {response_code}: {result_msg}")
+                return f"{response_code}: {result_msg}"
+            client_log.error(f"Data : {dict_from_server}")
+        return ""
+
+    @staticmethod
+    def presence(account_name, status):
+        data = {
+            "action": "presence",
+            "time": datetime.timestamp(datetime.now()),
+            "type": "status",
+            "user": {"account_name": account_name, "status": status},
+        }
+        return data
+
+    @staticmethod
+    def send_data(data, sock):
+        result_data = json.dumps(data).encode("utf-8")
+        sock.send(result_data)
+
+    @staticmethod
+    def message_to_server(message, account_name):
+        data = {
+            "action": "message",
+            "time": datetime.timestamp(datetime.now()),
+            "text": message,
+            "user": {
+                "account_name": account_name,
+            },
+        }
+        return data
+
+    def chat_read(self, sock):
+        while True:
+            try:
+                data = self.data_from_server(sock)
+                answer = self.read_answer(data)
+                print(answer)
+                client_log.info(f"Message received: {answer} ")
+            except Exception as err:
+                client_log.error(f"Some errors: {err}")
+                exit(1)
+
+    def chat_write(self, user, s):
+        while True:
+            message = input(f">>>{user}\n")
+            try:
+                self.send_data(self.message_to_server(message, user), s)
+            except Exception as err:
+                client_log.error(f"Some errors: {err}")
 
 
 @log
-def message_to_server(message, account_name):
-    data = {
-        "action": "message",
-        "time": datetime.timestamp(datetime.now()),
-        "text": message,
-        "user": {"account_name": account_name},
-    }
-    return data
-
-
-@log
-def data_from_server(sock):
-    received_data = sock.recv(10000000)
-    decoded_data = received_data.decode("utf-8")
-    result_data = json.loads(decoded_data)
-    if type(result_data) == dict:
-        return result_data
-    return {}
-
-
-@log
-def send_data(data, sock):
-    result_data = json.dumps(data).encode("utf-8")
-    sock.send(result_data)
+def main(addr, port, user, status):
+    client_log.info("client.py start...")
+    client_log.info(
+        f"Func get_addr_port...DONE, params = {addr}, {port}, {user}, {status}"
+    )
+    s = create_socket_client(addr, port)  # socket creation, because constraints
+    client = Client(s, user, status)
+    client.run()
 
 
 @log
@@ -70,10 +175,13 @@ def get_addr_port():
     parser.add_argument(
         "--status", action="store", dest="status", type=str, default="Constantly Ill"
     )
-    # is a flag factually
-    # parser.add_argument("--send", action="store", dest="send", type=int, default=0)
     client_log.debug("Function get_addr_port starting...")
-    return parser.parse_args()
+    args = parser.parse_args()
+    addr = args.addr
+    port = args.port
+    user = args.user
+    status = args.status
+    return args, addr, port, user, status
 
 
 @log
@@ -89,86 +197,5 @@ def create_socket_client(addr, port):
         exit(1)
 
 
-@log
-def read_answer(dict_from_server):
-    try:
-        response_code = dict_from_server.get("response", 0)
-    except Exception as err:
-        client_log.error(f"Error {err})")
-    else:
-        if response_code:
-            if response_code == 200:
-                result_msg = dict_from_server.get("alert", "OK.")
-                client_log.info("Response status OK")
-                try:
-                    result_msg = dict_from_server.get("text", 0)
-                except Exception as err:
-                    client_log.error(f"Error {err})")
-                else:
-                    if result_msg:
-                        return f"{result_msg[0]} by {result_msg[1]}"
-            else:
-                result_msg = dict_from_server.get("error", "unknown error")
-                client_log.info(f"Status: {response_code}: {result_msg}")
-            return f"{response_code}: {result_msg}"
-        client_log.error(f"Data : {dict_from_server}")
-    return ""
-
-
-def chat_read(sock):
-    while True:
-        try:
-            data = data_from_server(sock)
-            answer = read_answer(data)
-            print(answer)
-            client_log.info(f"Message received: {answer} ")
-        except Exception as err:
-            client_log.error(f"Some errors: {err}")
-            exit(1)
-
-
-def chat_write(user, s):
-    while True:
-        message = input(f">>>{user}\n")
-        try:
-            send_data(message_to_server(message, user), s)
-        except Exception as err:
-            client_log.error(f"Some errors: {err}")
-
-
-def run():
-    client_log.info("client.py start...")
-    args = get_addr_port()
-    addr, port, user, status = args.addr, args.port, args.user, args.status
-    client_log.info(f"parameters = {addr}, {port}, {user}, {status}")
-    s = create_socket_client(addr, port)
-    presence_msg = presence(user, status)
-    client_log.info("Presence message sending start...")
-    s.send(json.dumps(presence_msg, indent=4).encode("utf-8"))
-    client_log.info("Presence message sending...DONE")
-    # Slightly less mess of a msg parser
-    try:
-        data = data_from_server(s)
-        answer = read_answer(data)
-        print(f"Message(answer) from server received: ", answer)
-
-    except:
-        client_log.error("No messages from server.")
-        exit(1)
-
-    w = Thread(target=chat_write, args=(user, s))
-    w.daemon = True
-    w.start()
-
-    r = Thread(target=chat_read, args=(s,))
-    r.daemon = True
-    r.start()
-
-    while True:
-        time.sleep(1)
-        if not w.is_alive() or not r.is_alive():
-            break
-
-
 if __name__ == "__main__":
-    run()
+    main(*get_addr_port())
